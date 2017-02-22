5d6b76a1831340af3942798145839f5d9de6a767
Shuangshuang Zhao <poppindouble@gmail.com>
2017-02-22 17:03:08 -0500

    wip

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
 var totalPoints = 10;
 const k = 10;
 var particalSystem = [];
-var leafParticals = [];
+var hiddentParticals = [];
+var outsiderParticals = [];
 
 var maxNearbyParticals = 0;
 var minNearbyParticals = 0;
@@ -72,6 +73,51 @@ function setup() {
 
 	maxNearbyParticals = findMaxInArray(nearByParticalNumArr)
 	minNearbyParticals = findMinInArray(nearByParticalNumArr)
+
+	// making random dots inside the leaf
+
+	for(var i = 0; i < 350; i++) {
+		var golden_angle = PI * (sqrt(2));
+		var theta = i * golden_angle;
+		var radias = (sqrt(i) / sqrt(100)) * 380
+		var hiddenX = radias * cos(theta) + width / 2
+		var hiddenY = radias * sin(theta) + height / 2
+
+		var iX = floor(map(hiddenX, width / 2 - height / 2, width / 2 + height / 2, 0, myImage.width));
+		var iY = floor(map(hiddenY, 0, height, 0, myImage.height));
+		var r = myImage.pixels[(iX + iY * myImage.width) * 4];
+		// 	var g = myImage.pixels[(iX + iY) * myImage.width * 4 + 1];
+		// var b = myImage.pixels[(iX + iY) * myImage.width * 4 + 2];
+		if (r != 0) {
+			hiddentParticals.push(particalMaker(hiddenX , hiddenY))
+		}
+	}
+
+	// make 6 outsider dots for line
+	var outsiderPartical1X = floor((width / 2 - height / 2) / 2);
+	var outsiderPartical1Y = floor((height * 3 / 4) / 3);
+	outsiderParticals.push(particalMaker(outsiderPartical1X, outsiderPartical1Y));
+
+	var outsiderPartical2X = floor((width / 2 - height / 2) / 2);
+	var outsiderPartical2Y = floor((height * 3 / 4) * 2 / 3);
+	outsiderParticals.push(particalMaker(outsiderPartical2X, outsiderPartical2Y));
+
+	var outsiderPartical3X = floor((width / 2 - height / 2) / 2);
+	var outsiderPartical3Y = floor((height * 3 / 4));
+	outsiderParticals.push(particalMaker(outsiderPartical3X, outsiderPartical3Y));
+
+	var outsiderPartical4X = floor((width * 3 / 4 + height / 4));
+	var outsiderPartical4Y = floor((height * 3 / 4) / 3);
+	outsiderParticals.push(particalMaker(outsiderPartical4X, outsiderPartical4Y));
+
+	var outsiderPartical5X = floor((width * 3 / 4 + height / 4));
+	var outsiderPartical5Y = floor((height * 3 / 4) * 2 / 3);
+	outsiderParticals.push(particalMaker(outsiderPartical5X, outsiderPartical5Y));
+
+	var outsiderPartical6X = floor((width * 3 / 4 + height / 4));
+	var outsiderPartical6Y = floor((height * 3 / 4));
+	outsiderParticals.push(particalMaker(outsiderPartical6X, outsiderPartical6Y));
+
 }
 
 function particalMaker(x, y) {
@@ -85,6 +131,7 @@ function particalMaker(x, y) {
 		nearByParticalNum : 0
 	}
 }
+
 
 function updatePartical(p) {
 	var dx = p.targetX - mouseX;
@@ -100,10 +147,10 @@ function updatePartical(p) {
 	//var force = min(0.5, 1 / (sigma * sqrt(2 * PI)) * exp(-(d - mu)^2/(2 * sigma^2)))
 
 	var force;
-	if (d >= 10 && d < 100) {
+	if (d < 100) {
 		//force = min(0.5, 1 / (sigma * sqrt(2 * PI)) * exp(-(d - mu)^2/(2 * sigma^2)))
 		var k = 150
-		force = k / d
+		force = min(15, k / d)
 		var fX = dx/d * force;
 		var fY = dy/d * force;
 
@@ -145,6 +192,37 @@ function draw() {
 	background(255,255);
 	//background(myImage);
 
+	//plot all hidden particals
+	for(var i = 0; i < hiddentParticals.length; i++) {
+		var dx = hiddentParticals[i].targetX - mouseX;
+		var dy = hiddentParticals[i].targetY - mouseY;
+		var d = sqrt(dx * dx + dy * dy);
+		if (d < 20) {
+			noStroke()
+			fill(0,0,0)
+			ellipse(hiddentParticals[i].x, hiddentParticals[i].y, 5);
+
+
+			var minimunDistanceOutsiderPartical = outsiderParticals[0];
+			var tempDistanceX = outsiderParticals[0].x - hiddentParticals[i].x
+			var tempDistanceY = outsiderParticals[0].y - hiddentParticals[i].y
+			var minimunDistance = sqrt(tempDistanceX * tempDistanceX + tempDistanceY * tempDistanceY)
+
+			for(var j = 0; j < outsiderParticals.length; j++) {
+				var distanceX = outsiderParticals[j].x - hiddentParticals[i].x
+				var distanceY = outsiderParticals[j].y - hiddentParticals[i].y
+				var distance = sqrt(distanceX * distanceX + distanceY * distanceY)
+				if(distance < minimunDistance) {
+					minimunDistance = distance
+					minimunDistanceOutsiderPartical = outsiderParticals[j]
+					stroke(0)
+					line(minimunDistanceOutsiderPartical.x, minimunDistanceOutsiderPartical.y, hiddentParticals[i].x, hiddentParticals[i].y)
+				}
+			}
+		}
+	}
+
+
 	randomSeed(frameCount);
 	for(var i = 0; i < particalSystem.length; i++) {
 		var p = particalSystem[i];
@@ -153,26 +231,12 @@ function draw() {
 	randomSeed(0);
 	noStroke();
 
-	// for(var i = 0; i < particalSystem.length; i++) {
-	// 	var p = particalSystem[i];
-
-	// 	//var redColour = map(p.nearByParticalNum, minNearbyParticals, maxNearbyParticals, 150, 230);
-	// 	//fill(redColour,0,0);
-	// 	fill(255, 0, 0)
-
-	// 	translate(p.targetX, p.targetY)
-	// 	scale(randomX, randomY)
-	// 	rotate(angle);
-	// 	ellipse(0, 0, 3, 3)
-	// 	rotate(-angle);
-	// 	scale(1/randomX, 1 / randomY)
-	// 	translate(-p.targetX, -p.targetY)
-	// }
-
 	for(var i = 0; i < particalSystem.length; i++) {
 		var p = particalSystem[i];
 
 		var redColour = map(p.nearByParticalNum, minNearbyParticals, maxNearbyParticals, 120, 230) + random(-50, 30);
+
+		// two options here, leave at edge more darker, the other is more random
 		fill(redColour,0,0);
 		//fill(random(120, 240),0,0)
 		var triSize = 10;
@@ -182,7 +246,7 @@ function draw() {
 		var randomX = random(1 - variance, 1 + 2.5)
 		var randomY = random(1 - variance, 1 + 2.5)
 
-		if (p.nearByParticalNum < 10) {
+		if (p.nearByParticalNum < 8) {
 			randomX = 1
 			randomY = 1
 		}
